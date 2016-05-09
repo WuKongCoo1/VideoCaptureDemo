97cb1fa04d4ed7dd4a44284211d157032082c27b
WuKongCoo1 <wukong@wukedemac-mini.local>
2016-05-09 13:51:52 +0800

    Initial Commit

---
+++ VideoCaptureDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoCaptureDemo
+//
+//  Created by 吴珂 on 16/5/9.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoCaptureDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VideoCaptureDemo
+//
+//  Created by 吴珂 on 16/5/9.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
